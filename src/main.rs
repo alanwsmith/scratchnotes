0206c5d09f05b850fcfac435c7b5bdf2d591fc7a
Pol Fernandez <slushee@duck.com>
2023-06-30 17:03:43 +0200

    Add termimad

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,32 @@
-use clap::command;
-use clap::Arg;
-use dirs;
-use std::fs;
-use std::path::PathBuf;
+use clap::{command, Arg};
+use std::{fs, path::PathBuf, result::Result};
+use termimad::MadSkin;
 use walkdir::WalkDir;
 
 fn main() {
     let mut storage_dir = dirs::home_dir().unwrap();
     storage_dir.push(".h-files");
-    match verify_dir(&storage_dir) {
-        true => {
-            let matches = command!().arg(Arg::new("file")).get_matches();
-            match matches.get_one::<String>("file") {
-                Some(f) => {
-                    let mut file_path = storage_dir.clone();
-                    file_path.push(f);
-                    file_path.set_extension("txt");
-                    if file_path.exists() {
-                        let text = fs::read_to_string(file_path).unwrap();
-                        println!("-----------------------------------------");
-                        println!("{}", text);
-                        println!("-----------------------------------------");
-                    } else {
-                        println!("No file for: {}", f);
-                    }
-                }
-                None => {
-                    println!("Existing files:");
-                    list_files();
-                }
+    if verify_dir(&storage_dir) {
+        let matches = command!().arg(Arg::new("file")).get_matches();
+        if let Some(f) = matches.get_one::<String>("file") {
+            let mut file_path = storage_dir.clone();
+            file_path.push(f);
+            file_path.set_extension("md");
+
+            if file_path.exists() {
+                let text = fs::read_to_string(file_path).unwrap();
+                let skin = MadSkin::default();
+                skin.print_text(&text);
+            } else {
+                println!("No file for: {f}");
             }
+        } else {
+            println!("Existing files:");
+            list_files();
         }
-        false => {
-            println!("Could not make storage directory");
-            println!("{}", storage_dir.display());
-        }
+    } else {
+        println!("Could not make storage directory");
+        println!("{}", storage_dir.display());
     }
 }
 
@@ -42,10 +34,7 @@ fn verify_dir(dir: &PathBuf) -> bool {
     if dir.exists() {
         true
     } else {
-        match fs::create_dir_all(dir) {
-            Ok(_) => true,
-            Err(_) => false,
-        }
+        fs::create_dir_all(dir).is_ok()
     }
 }
 
@@ -55,7 +44,7 @@ pub fn list_files() {
     for entry in WalkDir::new(storage_dir)
         .sort_by_file_name()
         .into_iter()
-        .filter_map(|e| e.ok())
+        .filter_map(Result::ok)
     {
         if entry.path().is_file() {
             println!("{}", entry.path().file_stem().unwrap().to_str().unwrap());
